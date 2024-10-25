762a98918faead18f842191a5758185a5774b1d8
jahidul2004 <islamjahiduljihad@gmail.com>
2024-10-25 23:31:00 +0600

    Footer card section added.

--- src/componants/Footer/Footer.jsx
+++ src/componants/Footer/Footer.jsx
@@ -1,24 +1,24 @@
 const Footer = () => {
     return (
-        <div className="bg-[#06091a] text-white p-10 rounded-lg mb-10">
-            <div>
+        <div className="bg-[#06091a] text-white pt-20 pb-10 px-10 rounded-lg mb-10 relative">
+            <div className="mt-10">
                 <img
                     className="m-auto mb-5"
                     src="https://i.ibb.co.com/xqxYnD6/banner-main.png"
                     alt=""
                 />
             </div>
-            <div className="flex justify-between border-b border-[#777777] py-5">
+            <div className="flex flex-col md:flex-row lg:flex-row justify-between border-b border-[#777777] py-5">
                 <div>
                     <h1 className="font-bold text-lg mb-4">About Us</h1>
                     <p>
-                        We are a passionate team dedicated to providing the{" "}
-                        <br /> best services to our customers.
+                        We are a passionate team <br /> dedicated to providing
+                        the best <br /> services to our customers.
                     </p>
                 </div>
                 <div>
                     <h1 className="font-bold text-lg mb-4">Quick Links</h1>
-                    <ul className="flex flex-col gap-3">
+                    <ul className="flex flex-col gap-3 list-disc">
                         <a href="">
                             <li>Home</li>
                         </a>
@@ -36,7 +36,8 @@ const Footer = () => {
                 <div>
                     <h1 className="font-bold text-lg mb-4">Subscribe</h1>
                     <p className="mb-2">
-                        Subscribe to our newsletter for the latest updates.
+                        Subscribe to our newsletter for the <br /> latest
+                        updates.
                     </p>
                     <input
                         placeholder="Enter your Email"
@@ -50,6 +51,25 @@ const Footer = () => {
             </div>
             <div className="text-center mt-5">
                 <small>@2024 Your Company All Rights Reserved.</small>
+            </div>
+
+            <div className="w-[80%] text-center bg-gradient-to-tr from-[#b4e6f5] to-[#f9d995] p-10 absolute top-[-17%] left-1/2 transform -translate-x-1/2 rounded-xl">
+                <h1 className="text-2xl font-bold text-black">
+                    Subscribe to our Newsletter
+                </h1>
+                <p className="text-[#000000bd] my-2">
+                    Get the latest updates and news right in your inbox!
+                </p>
+                <div className="flex flex-col md:flex-row lg:flex-row gap-2 justify-center">
+                    <input
+                        type="text"
+                        placeholder="Enter Your Email"
+                        className="input"
+                    />
+                    <button className="btn bg-gradient-to-t from-[#dd84b5] to-[#f6cb69] border-none">
+                        Subscribe
+                    </button>
+                </div>
             </div>
         </div>
     );
--- src/componants/Players/Players.jsx
+++ src/componants/Players/Players.jsx
@@ -13,7 +13,7 @@ const Players = () => {
     }, []);
 
     return (
-        <div>
+        <div className="pb-20">
             <div className="mt-5 flex justify-between items-center my-2">
                 <h1 className="text-3xl font-bold">Available Players</h1>
                 <div className="flex gap-3 border p-2 rounded">
@@ -23,7 +23,7 @@ const Players = () => {
             </div>
 
             {/* Players Container */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10 pb-20">
                 {players.map((player) => (
                     <Player key={player.id} player={player}></Player>
                 ))}
